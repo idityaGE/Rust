25e400a93c93a1b0c55911bd9458643fb3944098
idityaGE <am44910606@gmail.com>
2024-06-25 21:25:41 +0530

    Update rustc_info.json with new rustc fingerprint

---
+++ setup/target/debug/deps/setup-36e01eaea06e41ef.d
@@ -0,0 +1,5 @@
+D:\Rust-🚀\setup\target\debug\deps\libsetup-36e01eaea06e41ef.rmeta: src/main.rs
+
+D:\Rust-🚀\setup\target\debug\deps\setup-36e01eaea06e41ef.d: src/main.rs
+
+src/main.rs:
---
+++ setup/target/debug/deps/setup-c00d07bcefce17fb.d
@@ -0,0 +1,5 @@
+D:\Rust-🚀\setup\target\debug\deps\libsetup-c00d07bcefce17fb.rmeta: src/main.rs
+
+D:\Rust-🚀\setup\target\debug\deps\setup-c00d07bcefce17fb.d: src/main.rs
+
+src/main.rs:
