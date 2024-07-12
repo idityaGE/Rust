22fcf2fc9d3cf4faded7dd612c6912e7a78ca14d
idityaGE <am44910606@gmail.com>
2024-07-12 20:47:11 +0530

    knfe

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let mut stack = vec![1, 2, 3, 4, 5];
-    while let Some(item) = stack.pop() {
-        println!("Popped item: {}", item);
+    let x = 5;
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        4 => println!("four"),
+        5 => println!("five"),
+        _ => println!("something")
     }
 }
